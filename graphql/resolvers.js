ffb9b8fa5c4c702c2a21ebbd5b061b452be9765e
jy <jaey5oung@naver.com>
2020-11-27 17:35:41 +0900

    graphQL Setting

---
+++ graphql/resolvers.js
@@ -0,0 +1,25 @@
+const people = [
+  {
+    name: "jangjaeyoung",
+    age: 19,
+    gender: "female",
+  },
+  {
+    name: "jangjae",
+    age: 18,
+    gender: "male",
+  },
+  {
+    name: "jang",
+    age: 17,
+    gender: "male",
+  },
+]
+
+const resolvers = {
+  Query: {
+    people: () => people,
+  },
+}
+
+export default resolvers
